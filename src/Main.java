88b83f6f3de77abe68b9ed1e50e6e2ab72263d1f
mishaf1203 <sduhHDFGH345>
2022-06-21 02:52:03 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,26 @@ public class Main {
         example[0] = 1;
         example[1] = 2;
         example[2] = 3;
-        System.out.println(example[0]);
-        System.out.println(example[1]);
-        System.out.println(example[2]);
+        String exampleString = Arrays.toString(example);
+        System.out.println(exampleString);
+//        System.out.println(example[0]);
+//        System.out.println(example[1]);
+//        System.out.println(example[2]);
 //1.2
         double[] realNum = {1.57, 7.654, 9,986 };
         String realNumString = Arrays.toString(realNum);
         System.out.println(realNumString);
 //1.3
         double [] aArray = {10.2, 1.42, 15.0};
-        System.out.println( aArray[0]);
-        System.out.println( aArray[1]);
-        System.out.println( aArray[2]);
+        String aArrayString = Arrays.toString(aArray);
+        System.out.println(aArrayString);
+//        System.out.println( aArray[0]);
+//        System.out.println( aArray[1]);
+//        System.out.println( aArray[2]);
 
 
-        //Задание 2
+
+
 
 
     }
