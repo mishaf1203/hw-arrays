144d578e3fc96ecc7a668cf9d071727dd70aab4a
mishaf1203 <sduhHDFGH345>
2022-06-21 02:48:14 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+//1.1
+        int [] example = new int[3];
+        example[0] = 1;
+        example[1] = 2;
+        example[2] = 3;
+        System.out.println(example[0]);
+        System.out.println(example[1]);
+        System.out.println(example[2]);
+//1.2
+        double[] realNum = {1.57, 7.654, 9,986 };
+        String realNumString = Arrays.toString(realNum);
+        System.out.println(realNumString);
+//1.3
+        double [] aArray = {10.2, 1.42, 15.0};
+        System.out.println( aArray[0]);
+        System.out.println( aArray[0]);
+        System.out.println( aArray[0]);
+
+        //Задание 2
+
+
+    }
+}
