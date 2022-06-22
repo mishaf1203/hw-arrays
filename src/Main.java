b110c56f135f81d33d2d36884cbe2ffb61adfcf0
mishaf1203 <sduhHDFGH345>
2022-06-22 21:14:44 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -9,25 +9,34 @@ public class Main {
         example[0] = 1;
         example[1] = 2;
         example[2] = 3;
-        String exampleString = Arrays.toString(example);
-        System.out.println(exampleString);
+
+        System.out.println(example[0]+ ", "+ example[1]+", " +example[2]);
 //        System.out.println(example[0]);
 //        System.out.println(example[1]);
 //        System.out.println(example[2]);
 //1.2
         double[] realNum = {1.57, 7.654, 9,986 };
-        String realNumString = Arrays.toString(realNum);
-        System.out.println(realNumString);
+        System.out.println(realNum[0] + ", " + realNum[1]+ ", " + realNum[2]);
 //1.3
         double [] aArray = {10.2, 1.42, 15.0};
-        String aArrayString = Arrays.toString(aArray);
-        System.out.println(aArrayString);
+
+        System.out.println(aArray[0] + ", " +aArray[1] + ", " +aArray[2] );
 //        System.out.println( aArray[0]);
 //        System.out.println( aArray[1]);
 //        System.out.println( aArray[2]);
 
         //Задание 3
-
+//        for (int i = exampleString; i > 0; i--){
+//
+//        }
+//        String example = "";
+//        for (int i : a){
+//            System.out.println(exampleString);
+//        }
+//        System.out.println(example);
+//        for (int i = example.length - 1; i >= 0; i--) {
+//            System.out.print();
+//        }
 
 
 
